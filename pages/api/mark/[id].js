0057eb6e868146f69fd020edce2fa2c5c982623c
lbuttignol <leandrobuttignol@gmail.com>
2020-06-04 23:03:44 -0300

    Integrate a Small SQLITE database :https://github.com/bmvantunes/youtube-2020-march-nextjs-part4

--- pages/api/mark/[id].js
+++ pages/api/mark/[id].js
@@ -1,14 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { markSquare, jumpTo } from '../../../redux/reducer'
+import sqlite from 'sqlite';
 
-export default (req, res) => {
+export default async (req, res) => {
   const {
     query: { id },
   } = req
-  
   // search into database the game id
   // mark square
   // Return game
 
-  res.status(200).json({ name: 'John Doe', value: id });
+  const db = await sqlite.open('./mydb.sqlite');
+  const people = await db.all('select * from person');
+
+  res.json(people);
 }
