d287c9d99205d3e7c6e12f88a209f706e11fbf0c
lbuttignol <leandrobuttignol@gmail.com>
2020-06-05 08:52:56 -0300

    Insert empty game is ready

--- pages/api/game.js
+++ pages/api/game.js
@@ -3,13 +3,10 @@ import sqlite from 'sqlite';
 
 export default async (req, res) => {
   const id =  uuidv4();
+  const emptyBoard = " , , , , , , , , ";
 
   const db = await sqlite.open('./mydb.sqlite');
-  console.log(await db.all('SELECT * FROM Game'));
-  const emptyBoard = " , , , , , , , , ";
-  console.log("id: " + id );
-  console.log("emptyBoard: " + emptyBoard );
-  const result = await db.exec('INSERT INTO Game VALUES (?,?)',[id.toString(),emptyBoard]);
+  const result = await db.run('INSERT INTO Game VALUES (?,?)',[id,emptyBoard]);
   await db.close();
 
   res.status(200).json({gameId: id});
