131be42653608ac8105f286da7672dcb6ea4fe72
lbuttignol <leandrobuttignol@gmail.com>
2020-06-09 11:14:47 -0300

    Define createGame

--- pages/api/game.js
+++ pages/api/game.js
@@ -2,22 +2,26 @@ import { v4 as uuidv4 } from 'uuid';
 import sqlite from 'sqlite';
 
 export default async (req, res) => {
+
+  const gId = await createGame();
+  console.log(gId);
+  res.status(200).json({gameId: gId});
+}
+
+async function createGame(){
   const gameId = uuidv4();
+  console.log(gameId);
   const boardId = uuidv4();
 
   const db = await sqlite.open('./mydb.sqlite');
   
-  // Set inital state
-  const xIsNext = true;
-  const winner = null;
   // insert Game
-  await db.run('INSERT INTO Game VALUES (?,?,?,?)',[gameId,xIsNext,winner,null]);
+  await db.run(`INSERT INTO Game ( id ) VALUES ( ? )`,[gameId]);
   // insert Board
-  const result = await db.run('INSERT INTO Board VALUES (?,?,?,?,?,?,?,?,?,?,?)',[boardId,null,null,null,null,null,null,null,null,null,gameId])
+  await db.run(`INSERT INTO Board ( id, gameId ) VALUES ( ?, ? )`,[boardId,gameId])
   // Insert Relationship
-  await db.run('UPDATE Game SET current = ? WHERE id = ?',[boardId,gameId]);
+  await db.run(`UPDATE Game SET current = ? WHERE id = ?`,[boardId,gameId]);
   
   await db.close();
-
-  res.status(200).json({gameId: gameId});
+  return gameId;
 }
