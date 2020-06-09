1242287001916d1719b33c5d44b19616d49570e6
lbuttignol <leandrobuttignol@gmail.com>
2020-06-09 15:11:33 -0300

    Brackets spaces

--- db/createDb.js
+++ db/createDb.js
@@ -2,13 +2,7 @@ const sqlite = require('sqlite');
 
 async function setup() {
     const db = await sqlite.open('./mydb.sqlite');
-    await db.migrate({force: 'last'});
-
-    const game = await sqlite.all('SELECT * FROM Game');
-    console.log('ALL PEOPLE', JSON.stringify(game));
-    
-    const Board = await sqlite.all('SELECT * FROM Board');
-    console.log('ALL PEOPLE', JSON.stringify(Board));
+    await db.migrate({ force: 'last' });
 }
 
 setup();
