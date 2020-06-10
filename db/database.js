77dce0daa8b8c80d5e19b7c7fd6676522a74e568
lbuttignol <leandrobuttignol@gmail.com>
2020-06-10 16:39:30 -0300

    gst

--- db/database.js
+++ db/database.js
@@ -53,7 +53,7 @@ class Database{
     return new Promise((resolve, reject) => {
       this.db.close((err) => {
         if (err){
-          console.log("Error closing Database");
+          console.log('Error closing Database');
           reject(err);
         }else{
           resolve();
