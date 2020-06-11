6ff29d89162478e073934dec2bff837c467fbdc0
lbuttignol <leandrobuttignol@gmail.com>
2020-06-11 09:57:13 -0300

    More Syntax changes

--- db/createDb.js
+++ db/createDb.js
@@ -5,16 +5,14 @@ require('dotenv').config();
 const schema = fs.readFileSync(process.env.DB_SCHEMA).toString();
 
 async function setup() {
-  
   const db = new sqlite3.Database(process.env.DB_NAME, (err) => {
     if (err) {
       console.log('Could not connect to database', err);
     }
-  })
-  
+  });
   // Convert the SQL string to array so that you can run them one at a time.
   const dataArr = schema.toString().split(';');
-
+  // Execute querys
   dataArr.forEach(query => {
     if (query) {
       // Add the delimiter back to each query before you run them
